76978336ffd0c3023366fc148d4872b02a048b54
gaelrobles22@gmail.com <ing.gaelgo@gmail.com>
2023-01-17 20:01:58 -0800

    Reduciendo Codigo

--- src/test/java/RegRestTests.java
+++ src/test/java/RegRestTests.java
@@ -1,25 +1,29 @@
 import io.restassured.RestAssured;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
-import org.junit.Before;
-import org.junit.Test;
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
 
 public class RegRestTests {
 
-    @Before
+    @BeforeEach
     public void setup(){
 
         RestAssured.baseURI = "https://reqres.in"; // El baseURI es el dominio sin el path, en este caso https://reqres.in
         RestAssured.basePath = "/api"; // El basePath es la palabra que sigue despues del host en este caso /api
+        RestAssured.filters(new RequestLoggingFilter(),new ResponseLoggingFilter());
     }
     @Test
     public void loginTest(){
 
         //Esta es una peticion POST y lleva un body de manera obligatoria y el metodo es .post en la URL
-        RestAssured
-                .given()
+                given()
                 .contentType(ContentType.JSON)
                 .body("{\n" +
                         "    \"email\": \"eve.holt@reqres.in\",\n" +
@@ -27,9 +31,7 @@ public class RegRestTests {
                         "}")
                 .post("login")
                 .then()
-                .log()
-                .all()
-                .statusCode(200)
+                .statusCode(HttpStatus.SC_OK)//HttpStatus nos sirve para pasarle el resultado esperado
                 .body("token",notNullValue());
 
        // System.out.println(response);
@@ -39,16 +41,11 @@ public class RegRestTests {
     public void getSingleUserTest(){
 
         //Esta es una peticion GET y no lleva body ya que extrae informacion y el metodo es tipo .get en la URL
-        RestAssured
-                .given()
-                .log()
-                .all()
+                 given()
                 .contentType(ContentType.JSON)
                 .get("users/2")
                 .then()
-                .log()
-                .all()
-                .statusCode(200)
+                .statusCode(HttpStatus.SC_OK)//HttpStatus nos sirve para pasarle el resultado esperado
                 .body("data.id",equalTo(2));
 
         // System.out.println(response);
